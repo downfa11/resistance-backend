a427534c1256063a838ddbb053e45e3966c03594
downfa11 <downfa11@naver.com>
2024-07-02 06:20:09 +0900

    chore: health check for k8s

---
+++ business-service/src/main/java/com/ns/business/adpater/in/web/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.ns.business.adpater.in.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/actuator")
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.status(HttpStatus.OK).body("healthy");
+    }
+}
---
+++ membership-service/src/main/java/com/ns/membership/adapter/in/web/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.ns.membership.adapter.in.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/actuator")
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.status(HttpStatus.OK).body("healthy");
+    }
+}
